4dcd1c25aebeac44062e6c3be148916eec1bbaab
pmunoz <pmunoz@dim.uchile.cl>
2018-11-27 15:14:02 -0300

    new test

--- test_calculator.py
+++ test_calculator.py
@@ -7,4 +7,3 @@ class TestCalculator:
         assert 1 == calculator.add(0, 1)
         assert 0 == calculator.add(0, 0)
         assert 0 == calculator.add(1, -1)
-        assert 0 == calculator.add(1, 1)
